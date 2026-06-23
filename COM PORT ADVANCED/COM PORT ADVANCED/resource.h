c28d69201691159692051a732cfab0c417c25323
Vladimir Astrelin <43417867+VladimirAstrelin@users.noreply.github.com>
2026-06-23 14:53:38 +0300

    Com Port Added

---
+++ COM PORT ADVANCED/COM PORT ADVANCED/resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by COM PORT ADVANCED.rc
+//
+#define IDD_MAIN_DIALOG                 101
+#define IDC_LIST_PORTS                  1001
+#define IDC_BTN_REFRESH                 1002
+#define IDC_BTN_CONNECT                 1003
+#define IDC_STATIC_STATUS               1005
+#define IDC_EDIT1                       1006
+#define ID_LOG                          1006
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
